c5fac1b2195cab153868c086b083f01e7930a52a
lijiajia <lijiajia3515@163.com>
2020-12-16 11:31:21 +0800

    这个故事还没有完结。——亚索

--- mongo/src/main/java/com/hfhk/cairo/starter/mongo/auditing/UidAuditorWare.java
+++ mongo/src/main/java/com/hfhk/cairo/starter/mongo/auditing/UidAuditorWare.java
@@ -4,6 +4,7 @@ package com.hfhk.cairo.starter.mongo.auditing;
 import com.hfhk.cairo.security.authentication.User;
 import com.hfhk.cairo.security.oauth2.server.resource.authentication.CairoAuthenticationToken;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
@@ -12,9 +13,17 @@ public class UidAuditorWare implements AuditorAware<String> {
 
 	@Override
 	public Optional<String> getCurrentAuditor() {
+
 		return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
-			.map(authentication -> (CairoAuthenticationToken) authentication)
-			.map(CairoAuthenticationToken::getUser)
-			.map(User::getUid);
+			.flatMap(authentication -> {
+				if (authentication instanceof CairoAuthenticationToken) {
+					return Optional.ofNullable(((CairoAuthenticationToken) authentication).getUser())
+						.map(User::getUid);
+				}
+				if (authentication instanceof AnonymousAuthenticationToken) {
+					return Optional.ofNullable(authentication.getPrincipal().toString());
+				}
+				return Optional.ofNullable(authentication.getPrincipal().toString());
+			});
 	}
 }
